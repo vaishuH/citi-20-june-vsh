5f059f638d4a6c7aae7a71a3afcb0951de8a822d
ibmpariwesh <pariweshg@gmail.com>
2022-06-20 12:01:02 +0530

    post requestcreate order

--- order-service/src/main/java/com/order/OrderController.java
+++ order-service/src/main/java/com/order/OrderController.java
@@ -2,6 +2,7 @@ package com.order;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("order")
 public class OrderController {
 	@PostMapping
-	public void createOrder() {
-		System.out.println("working");
+	public void createOrder(@RequestBody Order order) {
+		System.out.println(order.getPrice());
 	}
 	@GetMapping("/{id}")
 	public Order getOrder(Integer id) {
