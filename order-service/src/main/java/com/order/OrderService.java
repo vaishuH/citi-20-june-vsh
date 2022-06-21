93b11dd769e6f59185710748044b7f0e2b284b6a
ibmpariwesh <pariweshg@gmail.com>
2022-06-21 13:17:21 +0530

    eureka

--- order-service/src/main/java/com/order/OrderService.java
+++ order-service/src/main/java/com/order/OrderService.java
@@ -2,13 +2,18 @@ package com.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
+	@Autowired
+	RestTemplate template;
 	@Autowired
 	OrderRepository repository;
 
 	void insertOrder(OrderVO order) {
 		repository.save(order);
+		template.getForEntity("http://EMAIL-SERVICE/email/"+order.getEmail(), String.class);
+		
 	}
 }
